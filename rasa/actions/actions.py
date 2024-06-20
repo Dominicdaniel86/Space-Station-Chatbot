023e516e1fde775fcd06b098558a3005e170c3c5
Dominic Daniel Krämer <Dominic_Daniel.Kraemer@Student.Reutlingen-University.de>
2024-06-20 17:42:17 +0200

    Add basic python logic for the current_iss_location action

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -2,7 +2,10 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import os.path
+import urllib.request
 
+filepath = "../data/iss-location.txt"
 
 class ActionCurrentIssLocation(Action):
 
@@ -12,8 +15,24 @@ class ActionCurrentIssLocation(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        # download (and replace) file
+        if os.path.isfile(filepath):
+            os.remove(filepath)
+        urllib.request.urlretrieve("https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.txt", filepath)
 
-        dispatcher.utter_message(text="[Placeholder for the current ISS location]")
+        print(os.path.abspath(__file__))
+        file = open(filepath, "r")
+        end_reached = False
+        location = ""
+        for line in file:
+            if end_reached:
+                location = line
+                break
+            if "COMMENT End sequence of events" in line:
+                end_reached = True
+
+        dispatcher.utter_message(text="location: " + location)
 
         return []
 
@@ -25,6 +44,11 @@ class ActionFutureIssLocation(Action):
     def run(self, dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # download (and replace) file
+        if os.path.isfile("../../api-data/iss-location.txt"):
+            os.remove("../../api-data/iss-location.txt")
+        urllib.request.urlretrieve("https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.txt", "text.txt")
 
         if(len(tracker.latest_message['entities']) == 0):
             dispatcher.utter_message(text="I'm sorry, but I didn't understand your formatting. Please try again.")

dac4ee2166c0825be174ccd2f30b6cd519cc0d8e
Dominic Daniel Krämer <Dominic_Daniel.Kraemer@Student.Reutlingen-University.de>
2024-06-21 00:22:34 +0200

    Implement whole frontend

--- app/static/javascript/frontend.js
+++ app/static/javascript/frontend.js
@@ -1,15 +1,44 @@
 function sendMessage() {
     message = document.getElementById("inputField").value;
-    message = "message=" + message;
-    fetch("http://localhost:5000/sendmessage", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: message
-    })
-    .then((response) => response.json())
-    .then((json) => {
-        console.log(json[0].content);
-    });
+    if(message !== "") {
+
+        let newDiv = document.createElement("div");
+        newDiv.className = "message-user";
+
+        let newP = document.createElement("p");
+        newP.textContent = message;
+        newDiv.appendChild(newP);
+        let chat = document.getElementById("chat");
+        chat.insertBefore(newDiv, chat.firstChild);
+        // document.getElementById("chat").appendChild(newDiv);
+
+        message = "message=" + message;
+        document.getElementById('inputField').value = "";
+        fetch("http://localhost:5000/sendmessage", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: message
+        })
+        .then((response) => response.json())
+        .then((json) => {
+            let botMessage = json[0].content;
+            let newDiv = document.createElement("div");
+            newDiv.className = "message-bot";
+
+            let newP = document.createElement("p");
+            newP.textContent = botMessage;
+            newDiv.appendChild(newP);
+            let chat = document.getElementById("chat");
+            chat.insertBefore(newDiv, chat.firstChild);
+
+            // document.getElementById("chat").appendChild(newDiv);
+        });
+    }
 };
+
+function checkKeyInput(event) {
+    if(event.key === 'Enter')
+        sendMessage();
+}

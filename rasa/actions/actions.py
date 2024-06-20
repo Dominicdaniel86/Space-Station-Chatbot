c12db85a87724a4cb7dce967d8db1afa9588fedd
Dominic Daniel Krämer <Dominic_Daniel.Kraemer@Student.Reutlingen-University.de>
2024-06-20 16:38:09 +0200

    Add api call infrastructure

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -1,27 +1,35 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionCurrentIssLocation(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_current_iss_location"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="[Placeholder for the current ISS location]")
+
+        return []
+
+class ActionFutureIssLocation(Action):
+
+    def name(self) -> Text:
+        return "action_future_iss_location"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        if(len(tracker.latest_message['entities']) == 0):
+            dispatcher.utter_message(text="I'm sorry, but I didn't understand your formatting. Please try again.")
+        else:
+            dispatcher.utter_message(text="[Placeholder for the ISS location at: " + tracker.latest_message['entities'][0]['value'] + " ]")
+
+        return []
+
